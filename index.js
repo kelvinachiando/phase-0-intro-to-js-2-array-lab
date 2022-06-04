eb836979bbcc1f42c803d4e692ad25b4b49636b2
kelvinachiando <kelvinostin22gmail.com>
2022-06-04 18:06:58 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution here!
+const cats= ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat (){
+    cats.push('Ralph')
+}
+
+function destrcutivelyPrependcat (){
+    cats.unshift('Bob')
+}
+
